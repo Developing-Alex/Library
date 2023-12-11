10d0951a879c78aa63920e03796a2b876039c2d0
Alex <alexnowrang@gmail.com>
2023-12-11 06:46:54 -0600

    Create form to add new books

---
+++ app.js
@@ -0,0 +1,12 @@
+const myLibrary = [];
+
+function Book(title, author, yearPublished, read){
+  this.title = title;
+  this.author = author;
+  this.yearPublished = yearPublished;
+  this.read = read;
+}
+
+function addBookToLibrary(){
+
+}
