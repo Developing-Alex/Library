d7f8066f5ceb8b007b3438978837c7b653816075
Alex <alexnowrang@gmail.com>
2023-12-12 17:56:59 -0600

    Add function to store user input in array

--- app.js
+++ app.js
@@ -1,12 +1,55 @@
-const myLibrary = [];
+const myLibrary = [
+  /*{
+    title: "Invisible Man",
+    author: "Ralph Ellison",
+    year: 1952,
+    read: true,
+  },
+  {
+    title: "Harry Potter and the Sorcerer's Stone",
+    author: 'J.K. Rowling',
+    year: 1999,
+    read: true,
+  }*/
+];
 
-function Book(title, author, yearPublished, read){
+/*const form = document.querySelector('form');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const year = document.getElementById('year').value;
+    const read = document.getElementById('read').value;
+    const newBook = new Book(title, author, year, read);
+    myLibrary.push(newBook);
+  });*/
+
+function Book(title, author, year, read) {
   this.title = title;
   this.author = author;
-  this.yearPublished = yearPublished;
+  this.year = year;
   this.read = read;
 }
 
-function addBookToLibrary(){
+Book.prototype.info = function(){
+  return `${this.title} by ${this.author}, published in ${this.year} and ${this.read}.`
+  console.log(this.info);
+}
 
+function addBookToLibrary(obj) {
+  
+  myLibrary.push(obj)
+};
+
+addBookToLibrary(new Book('call', 'me', '1245', 'I have yet to read'))
+
+console.log(myLibrary)
+
+
+
+function displayBook(arrOfObjs) {
+  for (let i = 0; i < arrOfObjs.length; i++) {
+
+  }
 }
