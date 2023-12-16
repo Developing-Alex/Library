730295be9b049e3004bf993b9ea3e33d966d9b09
Alex <alexnowrang@gmail.com>
2023-12-15 22:04:47 -0600

    Fix app.js to disallow creation of duplicate cards

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const addBookBtn = document.getElementById('new-book-btn');
 const newBookForm = document.getElementById('new-book');
 const cardContainer = document.getElementById('card-container');
 const dialogModal = document.querySelector('dialog');
-let submitBtnClickCount = 0;
+let i = 0;
 
 const myLibrary = [];
-let newBook = {};
+let copyMyLibrary = [];
 
 function Book(title, author, year, read) {
   this.title = title;
@@ -26,8 +26,8 @@ function addBookToLibrary(newBook) {
 
 //This function renders books from an array to displayable cards on the UI//
 function displayBook(arrOfObjs) {
-  
-  //loops through array, creates cards and content elements//
+
+  //loops through array, creates cards and content elements for each index of the array//
   arrOfObjs.forEach(element => {
     const card = document.createElement("table");
     cardContainer.appendChild(card);
@@ -43,13 +43,13 @@ function displayBook(arrOfObjs) {
     cardContent.appendChild(cardYear);
     const cardRead = document.createElement("td");
     cardContent.appendChild(cardRead);
-    
+
     //fills cards with content//
     cardTitle.innerText = `"${element.title}"`;
     cardAuthor.innerText = `By: ${element.author}`;
     cardYear.innerText = `Published: ${element.year}`;
     cardRead.innerText = `Have you read this? ${element.read}`;
-    
+
     //styles cards//
     card.style.border = "3px solid black";
     card.style.backgroundColor = "whitesmoke"
@@ -62,19 +62,13 @@ function displayBook(arrOfObjs) {
     card.style.height = "300px"
     card.style.padding = "10px"
     cardTitle.style.marginTop = "0";
-    cardTitle.style.fontSize ="1.5rem"
+    cardTitle.style.fontSize = "1.5rem"
     cardContent.style.display = "flex";
     cardContent.style.flexDirection = "column";
     cardContent.style.alignItems = "flex-start";
     cardContent.style.justifyContent = "center";
     cardContent.style.gap = "10px";
     cardContent.style.fontSize = "1.2rem"
-
-    submitBtnClickCount++;
-  
-    if(submitBtnClickCount >= 1){
-    myLibrary.shift();
-  }
   });
 }
 
@@ -85,20 +79,42 @@ addBookBtn.addEventListener('click', () => {
 });
 
 //Takes user input from html form, creates Book objects using said data and pushes objects to myLibrary array//
-function inputNewBook(){
+function inputNewBook() {
   newBookForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const title = document.getElementById('title').value;
-  const author = document.getElementById('author').value;
-  const year = document.getElementById('year').value;
-  const read = document.getElementById('read').value;
-  newBook = new Book(title, author, year, read);
-  myLibrary.push(newBook);
-  displayBook(myLibrary);
-  console.log(newBook)
-});
 
-//Hides newBookForm and resets inputs//
+    event.preventDefault();
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const year = document.getElementById('year').value;
+    const read = document.getElementById('read');
+
+    const readIt = 'Yes';
+    const didntReadIt = 'No';
+    if (read.checked) {
+      addBookToLibrary(new Book(title, author, year, readIt));
+    } else {
+      addBookToLibrary(new Book(title, author, year, didntReadIt));
+    }
+   
+    //Allows cards to be created without duplicates being made
+    i++; 
+    if(i === 1){
+      console.log(i)
+      displayBook(myLibrary);
+    }else if(i === 2){
+      let shifted = myLibrary.shift();
+      console.log(i)
+      displayBook(myLibrary);
+      myLibrary.unshift(shifted);
+      console.log(myLibrary)
+    }else{
+      console.log(i)
+      copyMyLibrary = myLibrary.slice(i -1);
+      displayBook(copyMyLibrary);
+    }
+  });
+
+  //Hides newBookForm and resets inputs//
   newBookForm.addEventListener('submit', () => {
     title.value = '';
     author.value = '';
@@ -110,11 +126,10 @@ function inputNewBook(){
 };
 
 inputNewBook();
-
 /*
 **TO DO**
 --Remove last last enetered book as child from card container so duplicates dont arise//May have to use a click counter to achieve
 -- style form
---Style Style Style lol
+--Style 
 --Fix any other bugs that may pop up
 */
